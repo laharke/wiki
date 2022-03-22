aa155b021ec085b23fe75092288dfdf896e96c19
laharke <ronconimauro@yahoo.com.ar>
2022-03-22 17:59:54 -0300

    working wiki path

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,10 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry_page(request, name):
+    #necesito el NAME porque asi busco que quiero mostrar, basicamente en el path yo pongo str:name y el name del titulo va a ser lo que yo busque con al funcion
+    markdown = util.get_entry(name)
+    print(markdown)
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
