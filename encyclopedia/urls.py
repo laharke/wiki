915b2c87b57c73cc2b06bd08a8b045297ef79ae2
laharke <ronconimauro@yahoo.com.ar>
2022-03-23 18:21:22 -0300

    Entry page working, index page working, Search page working!

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:name>", views.entry_page, name="entry_page")
+    path("wiki/<str:name>", views.entry_page, name="entry_page"),
+    path("serach", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from . import util
 
@@ -10,8 +11,45 @@ def index(request):
 
 def entry_page(request, name):
     #necesito el NAME porque asi busco que quiero mostrar, basicamente en el path yo pongo str:name y el name del titulo va a ser lo que yo busque con al funcion
-    markdown = util.get_entry(name)
-    print(markdown)
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    if util.get_entry(name):
+        markdown = util.get_entry(name)
+        return render(request, "encyclopedia/entry.html", {
+            "entry": markdown,
+            'title': name
+        })
+    else:
+        messages.error(request, 'Entry not found')
+        return redirect('/')
+
+def search(request):
+    if request.GET['q'] == '':
+        messages.error(request, 'Type something in the search bar')
+        return redirect('/')
+    if request.method == "GET":
+        #si es get y no vacio puede ser dos cosas, o existe y muestro la pagina, o no existe y muestro resultados similares
+        #misma funcion que antes para pouplar la pagina y buscar la info
+        entry = request.GET['q'].lower()
+        if util.get_entry(entry):
+            markdown = util.get_entry(entry)
+            print(markdown)
+            return render(request, "encyclopedia/entry.html", {
+                "entry": markdown,
+                'title': entry
+        })
+        #si no se encontro nada, muestro simialres
+        else:
+            #Primero, agarro una lista ENTERA de todas las entry que tengo con la funcion auxiliar
+            list = util.list_entries()
+            similarList = []
+            # ahora tendria que loopear por esta lista, viendo si entry esta dentro de algunos de los elemetnos
+            for item in list:
+                itemLower = item.lower()
+                if entry in itemLower:
+                    print(entry, item)
+                    #ya tengo las similares, lo unico quetengo que hacer es meterlas en una lista para enviar al html SERACH
+                    similarList.append(item)
+            print(similarList)
+            messages.error(request, 'Acá hay similares')
+            return render(request, "encyclopedia/search.html", {
+                "entries": similarList,
+        })
